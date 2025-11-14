fdd435424b0ec38128f617dd31810c812ab1b227
alfian <h52981463h52981463235@gmail.com>
2025-11-14 20:37:39 +0700

    update public version

--- readme.txt
+++
@@ -1,3 +0,0 @@
-https://grok.com/share/c2hhcmQtMg%3D%3D_e31a1483-9cd7-4898-9ff1-91b0f07aa919
- 
- jangan dii rusak ya
--- script.js
+++ script.js
@@ -283,6 +283,9 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// refresions
+// https://grok.com/share/c2hhcmQtMg%3D%3D_e31a1483-9cd7-4898-9ff1-91b0f07aa919
+
 // === CONTACT FORM - Kirim ke Gmail ===
 document.getElementById('contactForm').addEventListener('submit', function(e) {
   e.preventDefault();
